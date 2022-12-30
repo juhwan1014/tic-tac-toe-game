8ae58b1e9c8a20c9df7fb0f5c583aef1dabfca4a
juhwan1014 <jmoon63@my.bcit.ca>
2022-12-30 11:15:09 -0800

    add explanation

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// 콜백 함수
+// A function that enters a parameter. 파라미터에 들어가는 함수
+//It is used when we want to execute the code sequentially. 코드를 순차저으로 시행하고 싶을 떄 사용한다.
+//ex) document.querySelector('.button').addEventListener('click', function(){ 블라블라 }) //클릭한 후 함수시행
+//ex2) setTimeout(function(){ 블라블라 }, 1000 )
+
+//여기서 질문이 생길 수 있다.
+
+//자바 스크립트는 synchronous language 동기적 언어 이기 떄문에 어차피(anyway) 순서대로 진행된다.
+//굳이, 콜백함수를 써야하는 이유는? 
+//예를들어 회사에 가서 A 라는 함수 뒤에 console.log("haha") 를 시행하고 싶어. 그런데 다른 사람은 console.log("hihi")를 시행하고 싶어
+//이렇게 여러 순서들이 겹칠 때에는 내 함수가 언제 어떻게 시행될지 장담하기 어렵다. 왜냐하면 함수의 크기(depending on function size)에 따라 시행속도(the speed of implementation)가 다르기 떄문이다.
+//그래서 어쨌든 
+
 window.addEventListener('DOMContentLoaded', () => {
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
@@ -5,11 +19,13 @@ window.addEventListener('DOMContentLoaded', () => {
     const announcer = document.querySelector('.announcer');
 
     let board = ['', '', '', '', '', '', '', '', ''];
+    // When the user clicked on the tile, it is to record the information about which user clicked. 
+    //유저카 타일을 클릭 할 떄, 어떤 유저가 클릭했는지를 기록
     let currentPlayer = 'X';
     let isGameActive = true;
 
-    const PLAYERX_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';
+    const PLAYER_X_WON = 'PLAYER_X_WON';
+    const PLAYER_O_WON = 'PLAYER_O_WON';
     const TIE = 'TIE';
 
 
@@ -48,7 +64,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     if (roundWon) {
-            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            announce(currentPlayer === 'X' ? PLAYER_X_WON : PLAYER_O_WON);
             isGameActive = false;
             return;
         }
@@ -59,10 +75,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const announce = (type) => {
         switch(type){
-            case PLAYERO_WON:
+            case PLAYER_O_WON:
                 announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
                 break;
-            case PLAYERX_WON:
+            case PLAYER_X_WON:
                 announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
                 break;
             case TIE:
@@ -78,10 +94,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
         return true;
     };
+    // It determines whether the clicked tile is already a selected tile or not.
+    //클릭된 타일이 이미 선택된 타일인지 아닌지를 결정하기 위함.
 
     const updateBoard =  (index) => {
         board[index] = currentPlayer;
     }
+    //아까 봤던 보드에 클릭되었다는 히스토리를 남기기 위해 
 
     const changePlayer = () => {
         playerDisplay.classList.remove(`player${currentPlayer}`);
@@ -92,8 +111,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive) {
-            tile.innerText = currentPlayer;
-            tile.classList.add(`player${currentPlayer}`);
+            tile.innerText = currentPlayer; //이터 텍스트 엠프티 하니간 현재 플레이어를 여기다 저장
+            tile.classList.add(`player${currentPlayer}`); //html 요소의 클래스 명을 바꾸거나 수정할때 쓰임
             updateBoard(index);
             handleResultValidation();
             changePlayer();
